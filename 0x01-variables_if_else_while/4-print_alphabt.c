44286abebedb556fc747f879240892e871560e3c
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-04-24 12:45:01 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
-i
+#include <stdio.h>
+/**
+ * main -main Entry point
+ * Description: 'When I was having that alphabet soup'
+ * Return: Always 0
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
