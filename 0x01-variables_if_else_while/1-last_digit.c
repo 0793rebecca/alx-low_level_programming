56f8309c09a45e5b24cbb04b32e6b636ec8d835c
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-04-24 09:47:07 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+printf("Last digit of %d is %d and is greater than 5\n", n % 10, ld);
 }
 else if (ld == 0)
 {
-printf("Last digit of %d is %d and is not 0\n", n, ld);
+printf("Last digit of %d is %d and is 0\n", n % 10, ld);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n % 10, ld);
 }
 return (0);
 }
