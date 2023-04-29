4d5113e5e9abf33db812b4f19a9db7ae130586da
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-04-29 03:56:17 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  *print _alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 int i;
 int j;
@@ -18,7 +17,4 @@ _putchar(j);
 }
 _putchar('\n');
 }
-}
-print_alphabet_x10();
-return (0);
 }
