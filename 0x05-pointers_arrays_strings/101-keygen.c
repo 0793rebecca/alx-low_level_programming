94a9367b97a72bcf8cdf814e4f39f6c5c1bcf119
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-05-02 16:46:53 -0700

    Don't hate the hacker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,7 @@ int main(void)
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	strand(time(0));
+	srand(time(0));
 
 	while (sum < 2772)
 
@@ -35,6 +35,7 @@ int main(void)
 
 		{
 			if (password[index] >= (33 + diff_half1))
+
 			{
 				password[index] -= diff_half1;
 
@@ -45,10 +46,10 @@ int main(void)
 		for (index = 0; password[index]; index++)
 		
 		{
-			if (password[index >= (33 + diff_half2))
+			if (password[index] >= (33 + diff_half2))
 
 			{
-				password[index -= diff_half2;
+				password[index] -= diff_half2;
 
 				break;
 
