fc61cd6f9739fb182d1c9a940aca6e77c33a5522
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-05-12 17:40:55 -0700

    A library is not a luxury

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * _puts - prints a string
+ * @str: string to be printed
  */
-int main(void)
+void _puts(char *str)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
