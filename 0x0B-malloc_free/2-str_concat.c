b3346c1d3ca64191242af8ab3c6f454d18aae566
rebecca alx <rebeccafrempongmensah@gmail.com>
2023-05-16 08:24:51 -0700

    He who is not courageous enough

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,43 +6,36 @@
  * str_concat - concatenates two strings
  * @s1: input one
  * @s2: input two
- *
  * Return: Null
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *b;
-
+	char *r;
 	int i, k;
-
 	if (s1 == NULL)
 
 		s1 = "";
-
 	if (s2 == NULL)
-
 		s2 = "";
-
 		i = k = 0;
-
 	while (s1[i] != '\0')
 		i++;
 	while (s2[k] != '\0')
 		k++;
-	b = malloc(sizeof(char) * (i + k + 1));
-	if (b == NULL)
+	r = malloc(sizeof(char) * (i + k + 1));
+	if (r == NULL)
 		return (NULL);
 	i = k = 0;
 	while (s1[i] != '\0')
 	{
-		b[i] = s1[i];
+		r[i] = s1[i];
 		i++;
 	}
 	while (s2[k] != '\0')
 	{
-		b[i] = s2[k];
-		i++, b++;
+		r[i] = s2[k];
+		i++, k++;
 	}
-	b[i] = '\0';
-	return (b);
+	r[i] = '\0';
+	return (r);
 }
